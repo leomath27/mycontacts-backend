67c8a24e9a5166bdcf996facb1657f031771ef83
Leon Mathews <126934482+leomath27@users.noreply.github.com>
2024-09-15 17:37:43 -0400

    Fix issue with multiple response headers and update token validation middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,16 +3,16 @@ const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async (req, res) => {
     
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({user_id: req.user.id});
     res.status(200).json(contacts);
 });
 
 //@desc Create New contacts
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is:", req.body);
     const{name, email, phone} = req.body;
@@ -24,14 +24,15 @@ const createContact = asyncHandler(async (req, res) => {
     const contact= await Contact.create({
         name,
         email,
-        phone
+        phone,
+        user_id: req.user.id
     });
     res.status(201).json(contact);
 });
 
 //@desc Get contact
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
@@ -43,12 +44,17 @@ const getContact = asyncHandler(async (req, res) => {
 
 //@desc Update Contact
 //@route PUT /api/contacts/:id
-//@access public
+//@access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
         res.status(404);
         throw new Error("contact not Found");
+    }
+
+    if(contact.user_id.toString() !==req.user.id){
+        res.status(403);
+        throw new Error("User doesnt have the permision to do this!");
     }
 
 
@@ -62,7 +68,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 //@desc Delete Contact
 //@route DELETE /api/contacts/:id
-//@access public
+//@access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if(!contact){
@@ -70,7 +76,12 @@ const deleteContact = asyncHandler(async (req, res) => {
         throw new Error("contact not Found");
     }
 
-    await Contact.deleteOne();
+    if(contact.user_id.toString() !==req.user.id){
+        res.status(403);
+        throw new Error("User doesnt have the permision to do this!");
+    }
+
+    await Contact.deleteOne({_id: req.params.id});
     res.status(200).json(contact);
 });
 
--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -6,13 +6,20 @@ const validateToken = asyncHandler(async(req,res, next) => {
     let authHeader = req.headers.Authorization || req.headers.authorization;
     if(authHeader && authHeader.startsWith("Bearer")){
         token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECTRET, (err, decoded)=>{
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
             if(err){
                 res.status(401);
                 throw new Error ("User is not authorized");
             }
-            console.log(decoded);
+            req.user = decoded.user;
+            next();
+
+           
         });
+        if (!token){
+            res.status(401);
+            throw new Error("User is not authorized or token is missing");
+        }
     }
 });
 
